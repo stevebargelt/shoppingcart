430a8cbfe7fd3cdbdf57b4aebd3229b3f5a542e8
Steve Bargelt <steve@bargelt.com>
2017-08-23 09:47:50 -0500

    Added Shopping Cart class + updated ShoppingCart Module to utilize ShoppingCart

--- src/shoppingcart/ShoppingCartModule.cs
+++ src/shoppingcart/ShoppingCartModule.cs
@@ -6,7 +6,24 @@ namespace shoppingcart
   {
       public ShoppingCartModule()
       {
-          Get("/shoppingcart/{userid:int}",  parameters => "This WILL be the shopping cart for userid " + parameters.userid);
+          Get("/shoppingcart/{userid:int}",  parameters => 
+          {
+              var userId = (int)parameters.userid;
+              if (userId == 123) 
+              {
+                var thisCart = new ShoppingCart(userId);
+                ShoppingCartItem item1 = new ShoppingCartItem(1, "Coffee", "Caffeine Delivery Sysytem", 12.99);  
+                ShoppingCartItem item2 = new ShoppingCartItem(2, "Heavy Cream", "Fat Delivery System", 4.99); 
+                thisCart.AddItem(item1);
+                thisCart.AddItem(item2);
+                return thisCart;
+              }
+              else
+              {
+                //if no cart found for that user we return a new, empty cart
+                return new ShoppingCart(userId);
+              }
+          });
       }
   }
 }
